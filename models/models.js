d5c9d2b14f4b10ea0080ae02c4799c5f22ae051a
John Robson <johnmkwork@gmail.com>
2022-11-14 14:16:21 +0000

    added error handle where if catagories is empty return 400 error

--- models/models.js
+++ models/models.js
@@ -2,6 +2,12 @@ const db = require("../db/connection.js");
 
 exports.selectCategories = () => {
   return db.query("SELECT * FROM categories").then((result) => {
+    if (result.rows.length === 0) {
+      throw {
+        status: 400,
+        msg: `No catagories`,
+      };
+    }
     return result.rows;
   });
 };
